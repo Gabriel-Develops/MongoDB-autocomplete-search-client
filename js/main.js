0d0f9d105f0261ab1d1c935fc1844f050a4a330f
Gabriel Rodriguez <garodri97@gmail.com>
2022-06-23 22:46:01 -0700

    style finished, added random button

--- js/main.js
+++ js/main.js
@@ -19,92 +19,107 @@ $(document).ready(() => {
             fetch(`https://mongodb-movie-db-sample.herokuapp.com/get/${ui.item.id}`)
                 .then(result => result.json())
                 .then(result => {
-                    console.log(result)
-                    // Empties out movie information
-                    $('#movie-title').empty()
-                    $('#movie-quick-info').empty()
-                    $('#movie-cast').empty()
-                    $('#imdb').empty()
-                    $('#imdb-rating').empty()
-                    $('#movie-production').empty()
-                    
-                    // Movie Title
-                    $('#movie-title').text(result.title)
-
-                    // Movie Date
-                    const releaseDate = new Date(result.released)
-                    $('#movie-quick-info').append(`<li>${releaseDate.getMonth()}/${releaseDate.getDate()}/${releaseDate.getFullYear()}</li>`)
-
-                    // Movie Genres (array)
-                    $('#movie-quick-info').append(`<li>${result.genres.join(', ')}</li>`)
-
-                    // Movie runtime
-                    let runtime = result.runtime
-                    $('#movie-quick-info').append(`<li>${Math.floor(runtime / 60)}h ${runtime % 60}m</li>`)
-
-                    // Movie cast       
-                    if (document.querySelector('#cast-tag').textContent === '')
-                        document.querySelector('#cast-tag').textContent = 'Cast'             
-                    result.cast.forEach(cast => $('#movie-cast').append(`<li>${cast}</li>`))
-
-                    // Movie poster
-                    $('#movie-poster').attr('src', result.poster)
-
-                    // Movie rating
-                    // imdb logo and link
-                    const imdbAnchor = document.querySelector('#imdb')
-                    const imdbLogo = document.createElement('img')
-                    imdbLogo.src = `https://img.icons8.com/color/100/000000/imdb.png`
-
-                    let imdbId = result.imdb.id.toString()
-                    while (imdbId.length < 7) {
-                        imdbId = '0' + imdbId
-                    }
-                    imdbAnchor.href = `https://www.imdb.com/title/tt${imdbId}/`
-
-
-                    imdbAnchor.append(imdbLogo)
-
-                    // imdb rating
-                    document.querySelector('#imdb-rating').textContent = `${result.imdb.rating}/10`
-
-                    // Movie plot
-                    if (document.querySelector('#production-tag').textContent === '')
-                        document.querySelector('#production-tag').textContent = 'Production'
-
-                    document.querySelector('#movie-plot').textContent = result.fullplot
-
-                    // Movie Production team
-                    // (directors and writers)
-                    if (document.querySelector('#plot-tag').textContent === '')
-                        document.querySelector('#plot-tag').textContent = 'Storyline'
-                    
-                    const production = {}
-
-                    if ('directors' in result)
-                        result.directors.forEach(director => production[director] = 'Director')
-
-                    if ('writers' in result) {
-                        result.writers.forEach(writer => {
-                            if (writer in production)
-                                return production[writer] = `${production[writer]}, Writer`
-                            else
-                                return production[writer] = 'Writer'
-                        })
-                    }
-                    // console.log(production)
-                    
-                    for (const producer in production) {
-                        const li = document.createElement('li')
-                        const pName = document.createElement('p')
-                        pName.textContent = producer
-                        const pTitle = document.createElement('p')
-                        pTitle.textContent = production[producer]
-
-                        li.append(pName, pTitle)
-                        document.querySelector('#movie-production').append(li)
-                    }
+                    movieToDom(result)
                 })
         }
     })
 })
+
+document.querySelector('#random').addEventListener('click', fetchRandom)
+
+async function fetchRandom() {
+    let response = await fetch('https://mongodb-movie-db-sample.herokuapp.com/rand')
+    let result = await response.json()
+    movieToDom(result)
+}
+
+function movieToDom(result) {
+    // Empties out movie information
+    $('#movie-title').empty()
+    $('#movie-quick-info').empty()
+    $('#movie-cast').empty()
+    $('#movie-image').empty()
+    $('#imdb').empty()
+    $('#imdb-rating').empty()
+    $('#movie-production').empty()
+    
+    // Movie Title
+    $('#movie-title').text(result.title)
+
+    // Movie Date
+    const releaseDate = new Date(result.released)
+    $('#movie-quick-info').append(`<li>${releaseDate.getMonth()}/${releaseDate.getDate()}/${releaseDate.getFullYear()}</li>`)
+
+    // Movie Genres (array)
+    $('#movie-quick-info').append(`<li>${result.genres.join(', ')}</li>`)
+
+    // Movie runtime
+    let runtime = result.runtime
+    $('#movie-quick-info').append(`<li>${Math.floor(runtime / 60)}h ${runtime % 60}m</li>`)
+
+    // Movie cast       
+    if (document.querySelector('#cast-tag').textContent === '')
+        document.querySelector('#cast-tag').textContent = 'Cast'             
+    result.cast.forEach(cast => $('#movie-cast').append(`<li>${cast}</li>`))
+
+    // Movie poster
+    const img = document.createElement('img')
+    img.src = result.poster
+    img.id = 'movie-poster'
+    document.querySelector('#movie-image').append(img)
+
+    // Movie rating
+    // imdb logo and link
+    const imdbAnchor = document.querySelector('#imdb')
+    const imdbLogo = document.createElement('img')
+    imdbLogo.src = `https://img.icons8.com/color/100/000000/imdb.png`
+
+    let imdbId = result.imdb.id.toString()
+    while (imdbId.length < 7) {
+        imdbId = '0' + imdbId
+    }
+    imdbAnchor.href = `https://www.imdb.com/title/tt${imdbId}/`
+
+
+    imdbAnchor.append(imdbLogo)
+
+    // imdb rating
+    document.querySelector('#imdb-rating').textContent = `${result.imdb.rating}/10`
+
+    // Movie plot
+    if (document.querySelector('#production-tag').textContent === '')
+        document.querySelector('#production-tag').textContent = 'Production'
+
+    document.querySelector('#movie-plot').textContent = result.fullplot
+
+    // Movie Production team
+    // (directors and writers)
+    if (document.querySelector('#plot-tag').textContent === '')
+        document.querySelector('#plot-tag').textContent = 'Storyline'
+    
+    const production = {}
+
+    if ('directors' in result)
+        result.directors.forEach(director => production[director] = 'Director')
+
+    if ('writers' in result) {
+        result.writers.forEach(writer => {
+            if (writer in production)
+                return production[writer] = `${production[writer]}, Writer`
+            else
+                return production[writer] = 'Writer'
+        })
+    }
+    // console.log(production)
+    
+    for (const producer in production) {
+        const li = document.createElement('li')
+        const pName = document.createElement('p')
+        pName.textContent = producer
+        const pTitle = document.createElement('p')
+        pTitle.textContent = production[producer]
+
+        li.append(pName, pTitle)
+        document.querySelector('#movie-production').append(li)
+    }
+}
